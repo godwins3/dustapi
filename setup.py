b6bb27e21db4d6a4575926a1068c102c01ae4dca
godwins3 <praisegodwins4@gmail.com>
2024-06-22 23:58:50 +0300

    create pyproject file

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='dust',
-    version='0.1',
+    version='0.1.0',
     packages=find_packages(),
     description='Fast light weight web server',
     author='c0d3g0d',
